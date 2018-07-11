b97aafa0d76b8f59803166c4a064836c7f00bf04
Sai Harin <sharin@homepointfinancial.com>
2018-07-10 23:08:47 -0400

    environment and package.json update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDVhA031xBOoEgJSHYZiHOEm3ZAEVu621w',
+    authDomain: 'qadashboard-f04d0.firebaseapp.com',
+    databaseURL: 'https://qadashboard-f04d0.firebaseio.com',
+    projectId: 'qadashboard-f04d0',
+    storageBucket: 'qadashboard-f04d0.appspot.com',
+    messagingSenderId: '1027978388369'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyDVhA031xBOoEgJSHYZiHOEm3ZAEVu621w',
+    authDomain: 'qadashboard-f04d0.firebaseapp.com',
+    databaseURL: 'https://qadashboard-f04d0.firebaseio.com',
+    projectId: 'qadashboard-f04d0',
+    storageBucket: 'qadashboard-f04d0.appspot.com',
+    messagingSenderId: '1027978388369'
+  }
 };
 
 /*
